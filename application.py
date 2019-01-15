f99619108c631af05037fd2630dc453c1ce71745
Joey <joeyhuitenga@hotmail.com>
2019-01-15 19:59:37 +0000

    Add existing file

--- application.py
+++ application.py
@@ -25,6 +25,9 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+# configure CS50 Library to use SQLite database
+db = SQL("sqlite:///finance.db")
+
 @app.route("/")
 def index():
     return render_template("index.html")
