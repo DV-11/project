741af46962aa1787884f55b55575c223ae5bb55f
J.M. van der Stoep <justmingo@gmail.com>
2019-01-23 12:08:33 +0000

    scroll

--- static/scroll.js
+++ static/scroll.js
@@ -3,7 +3,7 @@ $("img").slice(n,).hide()
 $("div.tekst").slice(n,).hide()
 
 function Showmore() {
-    n + 16
     $("img").slice(0,n).show()
-    $("div.tekst").slice(0,n).show();
+    $("div.tekst").slice(0,n).show()
+    n + 16;
 }
