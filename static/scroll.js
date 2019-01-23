831a1b6a5c1718a3b802106e697ebddf2b26629e
J.M. van der Stoep <justmingo@gmail.com>
2019-01-23 11:41:10 +0000

    scroll

--- static/scroll.js
+++ static/scroll.js
@@ -1,3 +1,21 @@
-$("img").slice(16,).hide()
+ document.getElementById("scroll-content").addEventListener("scroll", function (event) {
+     var newDiv = document.createElement("div");
+        newDiv.innerHTML = "my awesome new div";
+        document.getElementById("scroll-content").appendChild(newDiv);
+});
 
-$("div.tekst").slice(16,).hide()
+
+var checkForNewDiv = function () {
+    var lastDiv = document.querySelector("#scroll-content > div:last-child");
+    var maindiv = document.querySelector("#scroll-content");
+    var lastDivOffset = lastDiv.offsetTop + lastDiv.clientHeight;
+    var pageOffset = maindiv.offsetTop + maindiv.clientHeight;
+    if (pageOffset > lastDivOffset - 10) {
+        var newDiv = document.createElement("div");
+        newDiv.innerHTML = "my awesome new div";
+        document.getElementById("scroll-content").appendChild(newDiv);
+        checkForNewDiv();
+    }
+};
+
+checkForNewDiv();
