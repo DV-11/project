4576509c1df8bf94a09a065b32c05a3b7217ad8a
David Valero <david.valero.m@gmail.com>
2019-01-25 12:08:26 +0000

    remove

--- static/formValidation.js
+++
@@ -1,8 +0,0 @@
-function passCheck() {
-  $('#password, #confirmation').on('keyup', function () {
-  if ($('#password').val() == $('#confirmation').val()) {
-    $('#message').html('Confirmation and password match').css('color', 'green');
-  } else
-    $('#message').html('Confirmation does not match password').css('color', 'red');
-});
-}
